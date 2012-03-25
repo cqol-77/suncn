94dd7ea83184fc323c44c50aac64584e948a9571
mailzwj <mailzwj@126.com>
2012-03-25 17:29:02 +0800

    update html5 hack for IE

--- my_zen_settings.js
+++ my_zen_settings.js
@@ -1,7 +1,7 @@
 var my_zen_settings = {
 	'html': {
 		'abbreviations': {
-			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>',
+			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>',
 			'kissy': '<script type="text/javascript" src="http://a.tbcdn.cn/??s/kissy/1.1.6/kissy-min.js"></script>',
 			'yui': '<script type="text/javascript" src="http://a.tbcdn.cn/tbra/1.0/tbra-aio.js"></script>',
 			'a':'<a href="#|" target="_blank">',
@@ -12,7 +12,7 @@ var my_zen_settings = {
 		'snippets': {
 			'cc:ie8': '<!--[if gte IE 8]>\n\t${child}|\n<![endif]-->',
 			'cc:ie9': '<!--[if gte IE 9]>\n\t${child}|\n<![endif]-->',
-			'cc:html5':'<!--[if IE]>\n<script src="http://a.tbcdn.cn/p/fp/2011a/html5.js"></script>\n<![endif]-->'
+			'cc:html5':'<!--[if IE]>\n<style>\n/*html5*/\narticle,aside,dialog,footer,header,section,footer,nav,figure,menu{display:block;}\n</style>\n<script src="http://a.tbcdn.cn/p/fp/2011a/html5.js"></script>\n<![endif]-->'
 		}
 	},
 	'css': {
@@ -20,7 +20,7 @@ var my_zen_settings = {
 			"@md": "@-moz-document url-prefix() {\n\t${child}|\n}",
 			'bg+':'background:url(imgs/img_|) no-repeat 0 0 #fff;',
 			'bgi':'background-image:url(imgs/img_|);',
-			'bgc':'background-color:#fff;'
+			'bgc':'background-color:#fff;',
 		}
 	}
 };
