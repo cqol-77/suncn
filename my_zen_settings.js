2dc6afd847af1d076a250495a13b47b36a257c80
mailzwj <mailzwj@126.com>
2012-03-25 14:28:48 +0800

    DIY Zen Codding

---
+++ my_zen_settings.js
@@ -0,0 +1,26 @@
+var my_zen_settings = {
+	'html': {
+		'abbreviations': {
+			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>',
+			'kissy': '<script type="text/javascript" src="http://a.tbcdn.cn/??s/kissy/1.1.6/kissy-min.js"></script>',
+			'yui': '<script type="text/javascript" src="http://a.tbcdn.cn/tbra/1.0/tbra-aio.js"></script>',
+			'a':'<a href="#|" target="_blank">',
+			'a:mail':'<a href="mailto:|" target="_blank">',
+			'a:link':'<a href="http://|" target="_blank">',
+			'img':'<img src="imgs/img_|">'
+		},
+		'snippets': {
+			'cc:ie8': '<!--[if gte IE 8]>\n\t${child}|\n<![endif]-->',
+			'cc:ie9': '<!--[if gte IE 9]>\n\t${child}|\n<![endif]-->',
+			'cc:html5':'<!--[if IE]>\n<script src="http://a.tbcdn.cn/p/fp/2011a/html5.js"></script>\n<![endif]-->'
+		}
+	},
+	'css': {
+		'snippets': {
+			"@md": "@-moz-document url-prefix() {\n\t${child}|\n}",
+			'bg+':'background:url(imgs/img_|) no-repeat 0 0 #fff;',
+			'bgi':'background-image:url(imgs/img_|);',
+			'bgc':'background-color:#fff;'
+		}
+	}
+};
