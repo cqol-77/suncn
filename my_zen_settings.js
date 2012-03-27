9105969fe3b377180702fd27c6ccdd2900e8d2e2
mailzwj <mailzwj@126.com>
2012-03-27 16:05:06 +0800

    update bg:ie

--- my_zen_settings.js
+++ my_zen_settings.js
@@ -1,13 +1,16 @@
 var my_zen_settings = {
 	'html': {
 		'abbreviations': {
-			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>',
-			'kissy': '<script type="text/javascript" src="http://a.tbcdn.cn/??s/kissy/1.1.6/kissy-min.js"></script>',
-			'yui': '<script type="text/javascript" src="http://a.tbcdn.cn/tbra/1.0/tbra-aio.js"></script>',
+			'jq': '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>',
+			'kissy': '<script src="http://a.tbcdn.cn/??s/kissy/1.1.6/kissy-min.js"></script>',
+			'kissy:new':'<script src="http://a.tbcdn.cn/s/kissy/1.2.0/kissy-min.js"></script>',
+			'yui': '<script src="http://a.tbcdn.cn/tbra/1.0/tbra-aio.js"></script>',
 			'a':'<a href="#|" target="_blank">',
 			'a:mail':'<a href="mailto:|" target="_blank">',
 			'a:link':'<a href="http://|" target="_blank">',
-			'img':'<img src="imgs/img_|">'
+			'img':'<img src="imgs/img_|">',
+			'style':'<style>',
+			'script':'<script>',
 		},
 		'snippets': {
 			'cc:ie8': '<!--[if gte IE 8]>\n\t${child}|\n<![endif]-->',
@@ -19,6 +22,7 @@ var my_zen_settings = {
 		'snippets': {
 			"@md": "@-moz-document url-prefix() {\n\t${child}|\n}",
 			'bg+':'background:url(imgs/img_|) no-repeat 0 0 #fff;',
+			"bg:ie": "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='imgs/img_|',sizingMethod='crop');",
 			'bgi':'background-image:url(imgs/img_|);',
 			'bgc':'background-color:#fff;',
 		}
